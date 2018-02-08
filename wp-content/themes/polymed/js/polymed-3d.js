2c141cca5f97ab4ea5cf6178ec54ff2170fc3f1a
TheHunterSem <kiriljuk.vasja@gmail.com>
2018-02-08 12:56:59 +0200

    js href

--- wp-content/themes/polymed/js/polymed-3d.js
+++ wp-content/themes/polymed/js/polymed-3d.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function ($) {
 
+    $('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+        return false;
+    });
+
+    if (window.location.hash) {
+        scroll(0, 0);
+        // smooth scroll to the anchor id
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top + 'px'
+        }, 1500, 'swing');
+    }
+
     var plusBtn = $('.key-wrapper .key-block .plus-btn');
     var infoBlock = $(plusBtn).parent().parent().find('.key-info-block');
 
@@ -10,7 +25,7 @@ jQuery(document).ready(function ($) {
             $(this).removeClass('active');
         }
 
-        $(infoBlock).slideToggle();
+        $(infoBlock).stop().slideToggle();
 
     });
 
