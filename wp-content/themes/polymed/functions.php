0c662a05036e2334b011af35795117a61c482a53
TheHunterSem <kiriljuk.vasja@gmail.com>
2018-02-08 19:41:53 +0200

    fix bugs of task #8

--- wp-content/themes/polymed/functions.php
+++ wp-content/themes/polymed/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+//Custom cart button text
 add_filter( 'woocommerce_product_add_to_cart_text', 'woo_archive_custom_cart_button_text' );    // 2.1 +
 function woo_archive_custom_cart_button_text() {
     if(is_page('1635')){
