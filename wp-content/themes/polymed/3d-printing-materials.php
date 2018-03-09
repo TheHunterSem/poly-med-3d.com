fa7534ed324ad7c1cdbd25d943ab16e69a5ff6ca
HunterSem <vasyl.kyryliuk@elogic.com.ua>
2018-03-09 17:44:25 +0200

    fix responsive

--- wp-content/themes/polymed/3d-printing-materials.php
+++ wp-content/themes/polymed/3d-printing-materials.php
@@ -48,7 +48,7 @@
 
             <div class="second-item-block item-block-general">
                 <div class="img-block">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/filaments-2.png" alt="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/filaments-2-big.png" alt="">
                 </div>
                 <div class="content-block two">
                     <div class="content-item">
@@ -81,29 +81,37 @@
 
             <div class="third-item-block">
                 <div class="item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-1.png" alt="">
-                    <p class="text">Poly-Med is an FDA and ISO 13485 registered company that operates a manufacturing
-                        facility in compliance to 21 CFR Part 820, Quality System Regulation. Because we have the
-                        ability to see our products from raw monomer to extruded 3D filament, our quality assurance
-                        standards are engrained in the very core of our company.</p>
+                    <div class="content-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-1.png" alt="">
+                        <p class="text">Poly-Med is an FDA and ISO 13485 registered company that operates a manufacturing
+                            facility in compliance to 21 CFR Part 820, Quality System Regulation. Because we have the
+                            ability to see our products from raw monomer to extruded 3D filament, our quality assurance
+                            standards are engrained in the very core of our company.</p>
+                    </div>
                 </div>
                 <div class="item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-2.png" alt="">
-                    <p class="text">All 3D filaments are further inspected and assessed for molecular properties to
-                        ensure consistent and repeatable printing. Our materials are tested for molecular weight,
-                        monomer content, and thermal properties.</p>
+                    <div class="content-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-2.png" alt="">
+                        <p class="text">All 3D filaments are further inspected and assessed for molecular properties to
+                            ensure consistent and repeatable printing. Our materials are tested for molecular weight,
+                            monomer content, and thermal properties.</p>
+                    </div>
                 </div>
                 <div class="item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-3.png" alt="">
-                    <p class="text">As all of our bioresorbable polymers degrade by hydrolytic degradation, minimizing
-                        water content is of the utmost importance for enhancing 3D filament shelf life and maintaining
-                        functionality. All products are thoroughly dried to less than 1000 ppm moisture and hermetically
-                        packaged in foil.</p>
+                    <div class="content-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-3.png" alt="">
+                        <p class="text">As all of our bioresorbable polymers degrade by hydrolytic degradation, minimizing
+                            water content is of the utmost importance for enhancing 3D filament shelf life and maintaining
+                            functionality. All products are thoroughly dried to less than 1000 ppm moisture and hermetically
+                            packaged in foil.</p>
+                    </div>
                 </div>
                 <div class="item">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-4.png" alt="">
-                    <p class="text">The final product! Enjoy printing a first-in-class 3D filament that can be used for
-                        bioresorbable scaffold and device development on your own 3D printer!</p>
+                    <div class="content-block">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/making-filaments-4.png" alt="">
+                        <p class="text">The final product! Enjoy printing a first-in-class 3D filament that can be used for
+                            bioresorbable scaffold and device development on your own 3D printer!</p>
+                    </div>
                 </div>
             </div>
 
@@ -222,7 +230,8 @@
 
                             <?php } ?>
 
-                            <a class="more-info-about" href="/shop/">Request more info about <?php echo get_the_title(); ?></a>
+                            <a class="more-info-about" href="/shop/">Request more info
+                                about <?php echo get_the_title(); ?></a>
 
                         </div>
 
